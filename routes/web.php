7451b3ac8f56901d0d6fa80e0824c275d8671a35
thangngo.dev <thangngo130802@gmail.com>
2025-06-01 01:01:50 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\Backend\ConfigController;
 use App\Http\Controllers\Backend\DashboardController;
 use App\Http\Controllers\Backend\KeywordController;
 use App\Http\Controllers\Backend\ProductController;
+
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\ProductController as FrontendProductController;
 use Illuminate\Support\Facades\Route;
+
 
 
 Route::prefix('admin')->name('admin.')->group(function () {
